665062ae35b1dc19941154c7cc29c6f69684b0b9
Michal Vince <vince.michal@gmail.com>
2020-03-03 15:21:03 +0100

    block again

--- block.py
+++ block.py
@@ -9,6 +9,7 @@ s = ctx.socket(zmq.PULL)
 s.bind("tcp://0.0.0.0:{}".format(sys.argv[1]))
 print("blocking port {}".format(sys.argv[1]))
 while True:
-    msg = s.recv_json()
-    print(msg)
+    # msg = s.recv_json()
+    # print(msg)
+    time.sleep(1)
 
